7735d67c55d023640dd87fe1886a3de1fc1b7797
broncostooge <sjones.csuf@gmail.com>
2020-02-07 14:53:55 -0800

    Updates

---
+++ precache-manifest.341b5d8c7fffd808e1263639dc43534c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2b410a64c60e95e31875fcc6605a49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea98e787493f37726afd",
+    "url": "/monsters-rolodex/static/css/main.5ef20698.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea98e787493f37726afd",
+    "url": "/monsters-rolodex/static/js/main.79a4bbd9.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
